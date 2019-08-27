1e693ee93762f0377010e1331f199575e62d7b8a
Benjamin Janecke <Benjamin.Janecke@sinc.de>
2019-08-27 17:14:00 +0200

    Initial commit

---
+++ ChatApp/Startup.cs
@@ -0,0 +1,45 @@
+﻿using ChatApp.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseSignalR(routes => { routes.MapHub<ChatHub>("/chatHub"); });
+            app.UseMvc(ConfigureRoutes);
+
+            void ConfigureRoutes(IRouteBuilder routes)
+            {
+                routes.MapRoute(name: "default_route", template: "{controller=Index}/{action=Index}/{id?}");
+            }
+        }
+    }
+}
---
+++ ChatConsole/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ChatApp Console Client");
+            Chat().Wait();
+        }
+
+        private static async Task Chat()
+        {
+            var connection = new HubConnectionBuilder().WithUrl("http://localhost:55504/chatHub").Build();
+
+            await connection.StartAsync();
+            connection.On<string, string>("ReceiveMessage", OnMessageReceived);
+
+            Console.WriteLine("Enter your messages and press RETURN to submit, ESC to cancel.");
+
+            while (true)
+            {
+                var message = ReadLineWithCancel();
+                if (message == null)
+                {
+                    break;
+                }
+
+                await connection.InvokeAsync("SendMessage", "Console Client", message);
+                Console.WriteLine();
+            }
+        }
+
+        private static void OnMessageReceived(string user, string message)
+        {
+            if (user == "Console Client")
+            {
+                return;
+            }
+
+            Console.WriteLine($"{user} says {message}");
+        }
+
+        private static string ReadLineWithCancel()
+        {
+            string result = null;
+
+
+            //The key is read passing true for the intercept argument to prevent
+            //any characters from displaying when the Escape key is pressed.
+            var info = Console.ReadKey(true);
+            var buffer = new StringBuilder();
+            while (info.Key != ConsoleKey.Enter && info.Key != ConsoleKey.Escape)
+            {
+                Console.Write(info.KeyChar);
+                buffer.Append(info.KeyChar);
+                info = Console.ReadKey(true);
+            }
+
+            if (info.Key == ConsoleKey.Enter)
+            {
+                result = buffer.ToString();
+            }
+
+            return result;
+        }
+    }
+}
